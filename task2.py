1fcf1397e9b2c9fc7dc657fd835c5df185541731
ARZakirov <arzakirov@edu.hse.ru>
2026-01-17 21:04:21 +0900

    Добавлена ссылка и ключ API

---
+++ task2.py
@@ -0,0 +1,10 @@
+import requests
+
+# Введите ваш API-ключ OpenWeather
+API_KEY = "ВАШ_API_КЛЮЧ"
+
+# Ввод города от пользователя
+city = input("Введите название города: ")
+
+# URL API
+url = "https://api.openweathermap.org/data/2.5/weather"
