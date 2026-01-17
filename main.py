7b74552882bdfa8deda4cbb39955ee92116ac9a8
ARZakirov <arzakirov@edu.hse.ru>
2026-01-17 19:49:57 +0900

    добавлен вывод 5 постов

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ url = "https://jsonplaceholder.typicode.com/posts"
 
 # Отправляем GET-запрос
 response = requests.get(url)
+
+
+# Проверяем, что запрос успешен
+if response.status_code == 200:
+    posts = response.json()
+
+    # Выводим первые 5 постов
+    for post in posts[:5]:
+        print(f"Post ID: {post['id']}")
+        print(f"Title: {post['title']}")
+        print(f"Body: {post['body']}")
+        print("-" * 40)
+else:
+    print("Ошибка при получении данных:", response.status_code)
